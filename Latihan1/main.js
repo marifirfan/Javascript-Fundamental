f29ede6f155d32abf68972b5dc57ccb4498a7f82
Muhammad Arif Irfan <marifirfannn@gmail.com>
2024-03-06 04:03:29 +0700

    first commit

---
+++ Latihan1/main.js
@@ -0,0 +1,31 @@
+// alert('eyyooo')
+
+//untuk alert masukkan value
+// prompt('masukkan usia anda!')
+
+//variable
+// var a = 30
+// let y = 2
+// const usia = 1
+
+//const gabisa dirubah
+// console.log('usia kamu adalah ' + usia)
+// let usia = prompt('usia kamu adalah : ')
+// let umur = 30
+// if (umur <= 30) {
+//    umur = 'kamu masih '
+// }
+// else {
+//     umur = 'km dah tua'
+// }
+// alert(`${umur}`)
+
+// let namaGuru = ['rijal', 'malik', 'akbar']
+// namaGuru.push = ('dea', 'fikri')
+// namaGuru.shift()//menghapus di head atau awal
+// namaGuru.pop()//menghapus di akhir 
+
+// for (let i = 0; i <= 10; i = i + 1) {
+//     console.log('contoh')
+// }
+let i = 0
